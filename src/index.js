6479972d4cc00c23d067f2f232fe578ab4af1ef8
Lam Ngo <lam.ngo.qu@gmail.com>
2018-04-02 23:33:37 +0300

    Added Redux store and remove uneccessary files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import { todoReducer } from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(todoReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
+  
 registerServiceWorker();
