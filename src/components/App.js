6631eea59d4205c57f14abe889c5c9740b5d3f28
Lam Ngo <lam.ngo.qu@gmail.com>
2018-04-04 23:41:32 +0300

    Extracting TodoList from App.js into a separate file and make it a container for listing all todos

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import AddTodo from '../containers/AddTodo';
+import TodoList from '../containers/TodoList';
 
-const TodoList = ({ todos, completeTodo, deleteTodo }) =>
+const App = ({ todos, completeTodo, deleteTodo }) =>
   <div>
     <AddTodo />
-    <h3>Todo list:</h3>
-    <table>
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Description</th>
-          <th>Date</th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {todos.map( (todo,index) =>
-          <Todo todo={todo} key={index}
-            completeTodo={() => completeTodo(index)}
-            deleteTodo={() => deleteTodo(index)}
-          />
-        )}
-      </tbody>
-    </table>
+    <TodoList />
   </div>
-
-const Todo = ({ todo, completeTodo, deleteTodo }) =>
-  <tr>
-    <td>{todo.title}</td>
-    <td>{todo.description}</td>
-    <td>{todo.date}</td>
-    <td><button onClick={() => completeTodo()}>COMPLETE</button></td>
-    <td><button onClick={() => deleteTodo()}>DELETE</button></td>
-  </tr>
-
-const mapStateToProps = state => ({ todos: state })
-
-const mapDispatchToProps = dispatch => ({
-  completeTodo: index => dispatch({ type: 'COMPLETE_TODO', payload: index}),
-  deleteTodo: index => dispatch({ type: 'DELETE_TODO', payload: index}),
-})
-
-const App = connect(mapStateToProps, mapDispatchToProps)(TodoList);
 
 export default App;
---
+++ src/containers/TodoList.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const TodoList = ({ todos, completeTodo, deleteTodo }) =>
+  <div>
+    <h3>Todo list:</h3>
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Date</th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {todos.map( (todo,index) =>
+          <Todo todo={todo} key={index}
+            completeTodo={() => completeTodo(index)}
+            deleteTodo={() => deleteTodo(index)}
+          />
+        )}
+      </tbody>
+    </table>
+  </div>
+
+const Todo = ({ todo, completeTodo, deleteTodo }) =>
+  <tr>
+    <td>{todo.title}</td>
+    <td>{todo.description}</td>
+    <td>{todo.date}</td>
+    <td><button onClick={() => completeTodo()}>COMPLETE</button></td>
+    <td><button onClick={() => deleteTodo()}>DELETE</button></td>
+  </tr>
+
+const mapStateToProps = state => ({ todos: state })
+
+const mapDispatchToProps = dispatch => ({
+  completeTodo: index => dispatch({ type: 'COMPLETE_TODO', payload: index}),
+  deleteTodo: index => dispatch({ type: 'DELETE_TODO', payload: index}),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
