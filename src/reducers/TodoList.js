8f2b22daffd8094407b1d2b619b035fef2e1510f
Lam Ngo <lam.ngo.qu@gmail.com>
2018-04-06 00:05:41 +0300

    Extract TodoReducer to a separate file

---
+++ src/reducers/TodoList.js
@@ -0,0 +1,25 @@
+const todoList = (state = [
+  { title: "Title 1", description: "This is todo 1", date: "Date 1"},
+  { title: "Title 2", description: "This is todo 2", date: "Date 2"},
+  { title: "Title 3", description: "This is todo 3", date: "Date 3"},
+  { title: "Title 4", description: "This is todo 4", date: "Date 4"},
+  { title: "Title 5", description: "This is todo 5", date: "Date 5"},
+], action) => {
+  switch (action.type) {
+    case 'COMPLETE_TODO':
+      const completeIndex = action.payload;
+      console.log(`COMPLETE_TODO with index ${completeIndex}`);
+      return [ ...state.slice(0, completeIndex), ...state.slice(completeIndex+1)];
+    case 'DELETE_TODO':
+      const deleteIndex = action.payload;
+      console.log(`DELETE_TODO with index ${deleteIndex}`);
+      return [ ...state.slice(0, deleteIndex), ...state.slice(deleteIndex+1)];
+    case 'ADD_TODO':
+      console.log(`ADD_TODO with details ${action.payload}`);
+      return [ ...state, action.payload];
+    default:
+      return state;
+  }
+};
+
+export default todoList;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,30 +1,7 @@
 import { combineReducers } from 'redux';
+import todoList from './TodoList.js';
 
-const todoReducer = (state = [
-  { title: "Title 1", description: "This is todo 1", date: "Date 1"},
-  { title: "Title 2", description: "This is todo 2", date: "Date 2"},
-  { title: "Title 3", description: "This is todo 3", date: "Date 3"},
-  { title: "Title 4", description: "This is todo 4", date: "Date 4"},
-  { title: "Title 5", description: "This is todo 5", date: "Date 5"},
-], action) => {
-  switch (action.type) {
-    case 'COMPLETE_TODO':
-      const completeIndex = action.payload;
-      console.log(`COMPLETE_TODO with index ${completeIndex}`);
-      return [ ...state.slice(0, completeIndex), ...state.slice(completeIndex+1)];
-    case 'DELETE_TODO':
-      const deleteIndex = action.payload;
-      console.log(`DELETE_TODO with index ${deleteIndex}`);
-      return [ ...state.slice(0, deleteIndex), ...state.slice(deleteIndex+1)];
-    case 'ADD_TODO':
-      console.log(`ADD_TODO with details ${action.payload}`);
-      return [ ...state, action.payload];
-    default:
-      return state;
-  }
-};
-
-const doneReducer = (state = [
+const doneList = (state = [
   { title: "Title 6", description: "This is todo 6", date: "Date 6"},
   { title: "Title 7", description: "This is todo 7", date: "Date 7"},
   { title: "Title 8", description: "This is todo 8", date: "Date 8"},
@@ -46,6 +23,6 @@ const doneReducer = (state = [
 };
 
 export default combineReducers({
-  todoList: todoReducer,
-  doneList: doneReducer,
+  todoList,
+  doneList,
 });
