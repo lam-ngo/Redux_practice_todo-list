1382d7e0f0dcba81856e384df97423a431c0cbbc
Lam Ngo <lam.ngo.qu@gmail.com>
2018-04-07 10:54:36 +0300

    Modified AddTodo to receive users' input and add new todo

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -2,26 +2,35 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 const AddTodo = ({ dispatch }) => {
-  let input
+  let title = { value: ''}, description = { value: ''}, date = { value: ''}
 
   return (
     <div>
       <h3>Add a new todo:</h3>
       <div>
-        <input type="text" placeholder="Title"></input>
-        <input type="text" placeholder="Description"></input>
-        <input type="date"></input>
-        <button
-          onClick={() => dispatch({
-            type: 'ADD_TODO',
-            payload: {
-              title: "New title",
-              description: "This is a new todo",
-              date: "New date"
+        <form onSubmit={e => {
+            e.preventDefault()
+            if (!title.value || !description.value || !date.value) {
+              alert('No field should be empty!')
+              return
             }
-          })}>
-          ADD
-        </button>
+            dispatch({
+              type: 'ADD_TODO',
+              payload: {
+                title: title.value,
+                description: description.value,
+                date: date.value
+              }
+            })
+            title.value = ''
+            description.value = ''
+            date.value = ''
+          }}>
+          <input type="text" placeholder="Title" ref={node => title = node}></input>
+          <input type="text" placeholder="Description" ref={node => description = node}></input>
+          <input type="date" ref={node => date = node}></input>
+          <button type="submit">ADD</button>
+        </form>
       </div>
     </div>
   )
