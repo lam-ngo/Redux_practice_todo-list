6addc76fbb6d97828e3da2d4951d58351a200716
Lam Ngo <lam.ngo.qu@gmail.com>
2018-04-17 23:28:47 +0300

    Added 2 more properties to each todo: id and completed. Move all datas into the same object for easier management

--- src/data/index.js
+++ src/data/index.js
@@ -1,15 +1,14 @@
-export const todoList = [
+const todos = [
     { id: 1, title: "Title 1", description: "This is todo 1", date: "2018-04-07", completed: false },
     { id: 2, title: "Title 2", description: "This is todo 2", date: "2018-04-07", completed: false },
     { id: 3, title: "Title 3", description: "This is todo 3", date: "2018-04-07", completed: false },
     { id: 4, title: "Title 4", description: "This is todo 4", date: "2018-04-07", completed: false },
     { id: 5, title: "Title 5", description: "This is todo 5", date: "2018-04-07", completed: false },
-  ];
-
-export const doneList = [
     { id: 6, title: "Title 6", description: "This is todo 6", date: "2018-04-07", completed: true },
     { id: 7, title: "Title 7", description: "This is todo 7", date: "2018-04-07", completed: true },
     { id: 8, title: "Title 8", description: "This is todo 8", date: "2018-04-07", completed: true },
     { id: 9, title: "Title 9", description: "This is todo 9", date: "2018-04-07", completed: true },
     { id: 10, title: "Title 10", description: "This is todo 10", date: "2018-04-07", completed: true },
   ];
+
+export default todos;
