69086a065dd057a8764c5bc5536bf1286661f5b4
Lam Ngo <lam.ngo.qu@gmail.com>
2018-04-17 23:30:32 +0300

    Modified TodoList to use refactored actions creators

--- src/containers/TodoList.js
+++ src/containers/TodoList.js
@@ -1,43 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Todo from '../components/Todo';
-import { todoList_remove, doneList_add } from '../actions';
+import { todo_complete, todo_delete } from '../actions';
 import { Container, Table } from '../styles';
 
-const TodoList = ({ todos, completeTodo, deleteTodo }) =>
-  <div style={Container}>
-    <h3>Todo list</h3>
-    <table style={Table.table}>
-      <thead>
-        <tr style={Table.row}>
-          <th style={Table.column_title}>Title</th>
-          <th style={Table.column_description}>Description</th>
-          <th style={Table.column_date}>Date</th>
-          <th style={Table.column_button}></th>
-          <th style={Table.column_button}></th>
-        </tr>
-      </thead>
-      <tbody>
-        {todos.map( (todo,index) =>
-          <Todo todo={todo} key={index}
-            button1="COMPLETE"
-            button2="DELETE"
-            function1={() => {
-              completeTodo(todo)
-              deleteTodo(index)
-            }}
-            function2={() => deleteTodo(index)}
-          />
-        )}
-      </tbody>
-    </table>
-  </div>
+class TodoList extends Component {
+  render () {
+    const todos = this.props.todos
+      ? this.props.todos.filter(todo => todo.completed === false)
+      : [];
+    return (
+        <div style={Container}>
+          <h3>Todo list</h3>
+          <table style={Table.table}>
+            <thead>
+              <tr style={Table.row}>
+                <th style={Table.column_title}>Title</th>
+                <th style={Table.column_description}>Description</th>
+                <th style={Table.column_date}>Date</th>
+                <th style={Table.column_button}></th>
+                <th style={Table.column_button}></th>
+              </tr>
+            </thead>
+            <tbody>
+              {todos.map( (todo,index) =>
+                <Todo todo={todo} key={index}
+                  button1="COMPLETE"
+                  button2="DELETE"
+                  function1={() => {
+                    this.props.completeTodo(todo.id)
+                  }}
+                  function2={() => this.props.deleteTodo(todo.id)}
+                />
+              )}
+            </tbody>
+          </table>
+        </div>
+    );
+  }
+}
 
-const mapStateToProps = state => ({ todos: state.todoList })
+const mapStateToProps = state => ({ todos: state.todos })
 
 const mapDispatchToProps = dispatch => ({
-  completeTodo: todo => dispatch(doneList_add(todo)),
-  deleteTodo: index => dispatch(todoList_remove(index)),
+  completeTodo: id => dispatch(todo_complete(id)),
+  deleteTodo: id => dispatch(todo_delete(id)),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
